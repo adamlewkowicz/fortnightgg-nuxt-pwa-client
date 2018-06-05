50f7f957672c002d4afc1a5078a281dd210532b0
CheckthestatisticsBruv <34718571+CheckthestatisticsBruv@users.noreply.github.com>
2018-06-05 10:49:58 +0200

    default layout

---
+++ components/TheNavbar.vue
@@ -0,0 +1,129 @@
+<template>
+  <div id="header_wrapper">
+    <header>
+      <div id="logo">
+        <nuxt-link to="/">
+          <img src="@/assets/img/logo.jpg" alt="Fortnight website logo"/>
+        </nuxt-link>
+      </div>
+
+      <nav>
+        <ul>
+          <li><nuxt-link to="/">Homepage</nuxt-link></li>
+          <li><nuxt-link to="/items">Items</nuxt-link></li>
+          <li><nuxt-link to="/stats">Stats</nuxt-link></li>
+        </ul>
+      </nav>
+
+      <div id="user_finder">
+        <input type="text" v-model="nickname" @keyup.enter="findUser">
+        <button type="button" @click="findUser">.GG</button>
+      </div>
+    </header>
+      <div id="stripe"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      nickname: ''
+    }
+  },
+  methods: {
+    findUser() {
+      if (this.nickname != null) {
+        console.log(123)
+        this.$router.push(`/stats/${this.nickname}`);
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#header_wrapper {
+  background-color: #1b1c2e;
+  box-shadow: 0px 0px 70px 6px rgba(28, 30, 83, 1);
+  margin-bottom: 60px;
+}
+
+header {
+  height: 70px;
+  max-width: 960px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+}
+
+#logo img {
+  pointer-events: none;
+  user-select: none;
+  position: relative;
+  top: 3px;
+}
+
+nav {
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    align-content: center;
+  }
+}
+
+li {
+  height: 100%;
+  a {
+    color: white;
+    text-decoration: none;
+    padding: 25px;
+    height: 100%;
+    &:hover {
+      background-color: rgba(255,255,255,0.1);
+
+    }
+  }
+}
+
+#user_finder {
+  display: flex;
+}
+
+#user_finder input {
+  max-width: 200px;
+  height: 30px;
+  width: 100%;
+  color: white;
+  border: none;
+  padding: 0 15px;
+  background-color: #373971;
+  transition: background-color 1s ease;
+  transition: box-shadow 0.3s ease;
+  font-size: 14px;
+  border-radius: 25px;
+  outline: none;
+  float: right;
+  &:hover, &:focus {
+    background-color: #444798;
+	  box-shadow: 0px 0px 25px -4px rgba(68,51,233,0.54);
+  }
+}
+
+#user_finder button {
+  border: none;
+  background-color: transparent;
+  font-family: 'Open Sans';
+  font-weight: 800;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
+}
+
+#stripe {
+  height: 3px;
+  background: linear-gradient(to right, #1cb1ef 0%,#1aa1eb 26%,#490cbd 61%,#4d0dbf 100%);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,43 @@
 <template>
   <div>
+    <the-navbar/>
     <nuxt/>
   </div>
 </template>
 
+<script>
+import TheNavbar from '../components/TheNavbar';
+
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
+
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
+  font-family: "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 14px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
 }
 
-*, *:before, *:after {
+body {
+  margin: 0;
+  background-color: #1b1c2e;
+  color: white;
+  background-image: url("../assets/img/banner.jpg");
+  background-repeat: no-repeat;
+  background-position: center top 40px;
+}
+
+/* *, *:before, *:after {
   box-sizing: border-box;
   margin: 0;
-}
+} */
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
