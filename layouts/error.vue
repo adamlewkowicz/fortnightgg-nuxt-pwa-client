47e181aafeeee5ed01b4744b22bae1436c85aacd
adamlewkowicz <34718571+CheckthestatisticsBruv@users.noreply.github.com>
2018-08-08 18:12:27 +0200

    custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Back to homepage</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'default'
+}
+</script>
+
+<style scoped>
+div {
+ text-align: center;
+}
+a {
+  color: #fff;
+}
+</style>
